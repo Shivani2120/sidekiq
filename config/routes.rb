05aea39d6bb77741216b333d75d849f9a8da8e21
Gourav <gauravdec25@gmail.com>
2022-06-01 19:37:37 +0530

    side kiq

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'endangered/index'
+  get 'endangered/data', to: 'endangered#data'
+  post 'endangered/upload', to: 'endangered#upload'
+  post 'endangered/destroy', to: 'endangered#destroy'
+  resources :sharks 
+  root 'home#index'
+  get 'home/index'
+end
